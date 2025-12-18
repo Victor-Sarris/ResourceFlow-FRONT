6c627d8bbbe0e37fef3c67419e021c298ad307d8
Victor Sarrís Silva Santos <devvictorsarris@gmail.com>
2025-12-18 16:28:58 -0300

    :zap: add home page

--- src/pages/Default.jsx
+++ src/pages/Default.jsx
@@ -1,46 +1,111 @@
-// import tailwindcss from "@tailwindcss/vite";
-import './Default.css'
-import Logo from '../assets/Resource-Flow.png'; 
-import { useNavigate } from 'react-router-dom';
+import Logo from "../assets/Resource-Flow.png";
+import { useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 
-function DefaultPage(){
+// Importacoes de particulas
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadFull } from "tsparticles";
+import particlesConfig from "../services/particles-config";
 
+function DefaultPage() {
   const navigate = useNavigate();
   const handleSelectLogin = () => {
-    navigate('/Login');
-  }
+    navigate("/Login");
+  };
   const handleSelectSingUp = () => {
-    navigate('/Cadastro');
-  }
+    navigate("/Cadastro");
+  };
 
-    return (
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadFull(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  return (
     <>
-    <img src={Logo} alt="" className='logo-img' />
-      <div className='background'>
-        <div className='main'>
-          <h1>Bem vindo(a) ao Resource Flow!</h1>
-          <div className='sub-content'>
-            <p>Uma plataforma intuitiva e simples para o agendamento de eventos em uma instituição de ensino.</p>
-            <p>Comece fazendo o login na plataforma 🙂👇</p>
-          </div>
-          <div className='role-selection'>
-            <button className='sing-up' onClick={handleSelectLogin}>Cadastrar-se</button>
-            <button className='login' onClick={handleSelectSingUp}>Fazer Login</button>
+      {/* Inicalizacao de particulas */}
+      {init && (
+        <Particles
+          id="tsparticles"
+          options={particlesConfig}
+          className="absolute top-0 left-0 w-full h-full z-0 pointer-events-none"
+        />
+      )}
+      {/* Configurações gerais da página */}
+      <div className="min-h-screen bg-[rgba(58,37,193,0.388)] text-white text-center flex flex-col items-center bg relative z-10">
+        {/* Logo */}
+        <img src={Logo} alt="" className="h-96 w-96 object-contain" />
+
+        {/* Container background */}
+        <div className="">
+          <div className="background">
+            {/* Conteudo principal */}
+            <div className="flex flex-col items-center justify-center">
+              <h1 className="font-bold ">
+                Bem vindo(a) ao Resource Flow!
+              </h1>
+
+              {/* Sub-conteudo */}
+              <div className="m-5">
+                <p>
+                  Uma plataforma intuitiva e simples para o agendamento de
+                  eventos em uma instituição de ensino.
+                </p>
+                <p>Comece fazendo o login na plataforma 🙂👇</p>
+              </div>
+
+              {/* Botoes */}
+              <div className="flex p-5 g-5 space-x-5">
+                <button className="bg-sky-600/80" onClick={handleSelectLogin} >
+                  Cadastrar-se
+                </button>
+                <button className="bg-emerald-600/80" onClick={handleSelectSingUp}>
+                  Fazer Login
+                </button>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-      <div className='footer'>
-        <div className='footer-content'>
-          <h3>&copy; todos os direitos reservados</h3>
-          <div className='footer-nav'>
-            <a href="https://victorsarris.netlify.app/" target='blank' rel='external'>Meu portifólio</a>
-            <a href="https://www.linkedin.com/in/victorsarris/" target='blank' rel='external'>Linkedin</a>
-            <a href="https://github.com/Victor-Sarris">GitHub</a>
+
+        <div className="flex flex-col mt-10 mb-5">
+          <div className="footer-content ">
+            <h3 className="">&copy; Victor S. | 🔱🪽, todos os direitos reservados</h3>
+            <div className="mt-2.5 space-x-2.5">
+              <a
+                href="https://victorsarris.netlify.app/"
+                target="blank"
+                rel="external"
+                className="text-white no-underline mx-2.5 hover:underline"
+              >
+                Meu portifólio
+              </a>
+              <a
+                href="https://www.linkedin.com/in/victorsarris/"
+                target="blank"
+                rel="external"
+                className="text-white no-underline mx-2.5 hover:underline"
+              >
+                Linkedin
+              </a>
+              <a
+                href="https://github.com/Victor-Sarris"
+                target="blank"
+                external="external"
+                className="text-white no-underline mx-2.5 hover:underline"
+              >
+                GitHub
+              </a>
+            </div>
           </div>
         </div>
       </div>
     </>
-  )
+  );
 }
 
 export default DefaultPage;
